0b05bda212447f67e9c5c85a1b8e7b8b3e19dd84
jorbit22 <jorbit173@gmail.com>
2024-04-14 04:29:29 +0100

    api1

--- backend/frontend/src/components/Sidebar.js
+++ backend/frontend/src/components/Sidebar.js
@@ -52,7 +52,7 @@ function Sidebar() {
   });
 
   function getRooms() {
-    fetch("http://localhost:5001/rooms")
+    fetch("https://chat-app-nnvy.onrender.com/rooms")
       .then((res) => res.json())
       .then((data) => setRooms(data));
   }
